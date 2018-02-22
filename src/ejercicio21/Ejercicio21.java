f17eb97180a896f6d693948ca5b8c84f9c4a71a5
arturo <arturo@ArturoMSI>
2018-02-22 14:38:26 +0100

    Proyecto Terminado

--- src/ejercicio21/Ejercicio21.java
+++ src/ejercicio21/Ejercicio21.java
@@ -10,7 +10,10 @@ public class Ejercicio21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Palabras pal=new Palabras();
+        pal.listaPalabras();
+        pal.ordenarLista();
+        pal.escribirFicheiro();
     }
 
 }
